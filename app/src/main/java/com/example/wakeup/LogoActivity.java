62c00fb289304452bba0d064a5a1a325265569a6
heeji <ghj2366@naver.com>
2021-06-05 02:51:20 +0900

    main activity_logo modify

---
+++ app/src/main/java/com/example/wakeup/LogoActivity.java
@@ -0,0 +1,31 @@
+package com.example.wakeup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceStare) {
+        super.onCreate(savedInstanceStare);
+        setContentView(R.layout.activity_logo);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
